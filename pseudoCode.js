262052ad9f1917690f583c3402e6b3b75d62e712
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-06-27 10:40:17 -0600

    added psuedoCode

---
+++ pseudoCode.js
@@ -0,0 +1,9 @@
+//assume we know the decimal of the book were looking for
+//walk down the shelfs until we find the first decimal point
+//once we find the decimal point we walk down the shelf until we find the book 
+//with the second decimal point
+//so forth until you get to the last decimal point
+//you should be at the book
+
+//this is a linear search algorithm because you cant teleport wherever you
+//and you afraid of librarians
